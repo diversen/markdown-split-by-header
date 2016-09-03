a5393403d4976106322ed7855186c74d626ae684
Dennis Iversen <dennis.iversen@gmail.com>
2016-09-03 15:19:08 +0200

    auto commit

--- test.php
+++ test.php
@@ -5,8 +5,13 @@ include_once "split.php";
 $text = file_get_contents('README.md');
 
 $s = new \diversen\markdownSplit();
-$res = $s->splitMarkdownAtLevel($text, true, 2);
 
-
-
+// Split by header level 1
+$res = $s->splitMarkdownAtLevel($text, true, 1);
 print_r($res);
+
+// Or split by header 2
+$res = $s->splitMarkdownAtLevel($text, true, 2);
+print_r($res);
+
+`Notice` $ret[0] is always any text that may be `before the first header` - this is normally empty.
